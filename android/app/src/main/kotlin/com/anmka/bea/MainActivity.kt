80c3a464f7c66dccf3f59e49d6931a924df6297a
Ibrahim Atef <atf343069@gmail.com>
2026-01-12 12:57:05 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/anmka/bea/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.anmka.bea
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+void main() {
+  runApp(const BeaApp());
+}
+
+class BeaApp extends StatelessWidget {
+  const BeaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BEA - British Egyptian Academy',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const WebViewScreen(),
+    );
+  }
+}
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController _controller;
+  double _loadingProgress = 0.0;
+  bool _isLoading = true;
+  String? _errorMessage;
+  bool _hasLoadedSuccessfully = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeWebView();
+  }
+
+  void _initializeWebView() {
+    late final PlatformWebViewControllerCreationParams params;
+
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else if (WebViewPlatform.instance is AndroidWebViewPlatform) {
+      params = AndroidWebViewControllerCreationParams();
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(params);
+
+    // Configure Android WebView settings
+    if (controller.platform is AndroidWebViewController) {
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.transparent)
+      ..enableZoom(true)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            setState(() {
+              _loadingProgress = progress / 100;
+            });
+          },
+          onPageStarted: (String url) {
+            setState(() {
+              _loadingProgress = 0.0;
+              _isLoading = true;
+              _errorMessage = null;
+            });
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              _loadingProgress = 1.0;
+              _isLoading = false;
+              _hasLoadedSuccessfully = true;
+              _errorMessage = null;
+            });
+          },
+          onWebResourceError: (WebResourceError error) {
+            if (!_hasLoadedSuccessfully &&
+                !error.description.contains('ERR_NAME_NOT_RESOLVED') &&
+                !error.description.contains('ERR_CONNECTION_REFUSED') &&
+                !error.description.contains('ERR_INTERNET_DISCONNECTED')) {
+              setState(() {
+                _errorMessage = 'Error loading page: ${error.description}';
+                _isLoading = false;
+              });
+            }
+            debugPrint('WebView Error: ${error.description}');
+          },
+          onNavigationRequest: (NavigationRequest request) async {
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://bea.anmka.com/'));
+
+    _controller = controller;
+  }
+
+  void _refreshWebView() {
+    setState(() {
+      _loadingProgress = 0.0;
+      _isLoading = true;
+      _errorMessage = null;
+      _hasLoadedSuccessfully = false;
+    });
+    _controller.reload();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) async {
+        if (!didPop) {
+          final canGoBack = await _controller.canGoBack();
+          if (canGoBack) {
+            _controller.goBack();
+          } else {
+            SystemNavigator.pop();
+          }
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: RefreshIndicator(
+            onRefresh: () async {
+              _refreshWebView();
+            },
+            child: Stack(
+              children: [
+                WebViewWidget(controller: _controller),
+                if (_isLoading && _loadingProgress < 1.0)
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: LinearProgressIndicator(
+                      value: _loadingProgress,
+                      backgroundColor: Colors.grey[200],
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.blue[700]!),
+                      minHeight: 3,
+                    ),
+                  ),
+                if (_errorMessage != null && !_hasLoadedSuccessfully)
+                  Center(
+                    child: Container(
+                      margin: const EdgeInsets.all(20),
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.red[50],
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(color: Colors.red[200]!),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.error_outline,
+                            color: Colors.red[600],
+                            size: 48,
+                          ),
+                          const SizedBox(height: 16),
+                          Text(
+                            _errorMessage!,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.red[700],
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          ElevatedButton.icon(
+                            onPressed: _refreshWebView,
+                            icon: const Icon(Icons.refresh),
+                            label: const Text('Retry'),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red[600],
+                              foregroundColor: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
